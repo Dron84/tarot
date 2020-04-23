a2795a8f80018ca22cd1652d9ba9c15451745e30
Dron84 <dron84@gmail.com>
2020-04-23 10:41:57 +0300

    add auto nav link

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Индивидуальный</router-link> |
-      <router-link to="/duble">Парный</router-link>
+      <router-link class="nav-link" :to="link.path" v-for="link in links" :key="link.path">{{link.title}}</router-link>
     </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+  export default {
+      data:()=>({
+          links: [],
+      }),
+      created(){
+          this.links = this.$router.options.routes
+      }
+  }
+</script>
 <style lang="sass">
   @import "sass/vars"
   #app
@@ -24,7 +32,15 @@
       font-weight: bold
       color: $fore
       text-decoration: none
+      position: relative
+      margin: 0 30px 0 0
       &.router-link-exact-active
         color: $five
         text-decoration: underline
+      &::after
+        content: "|"
+        position: absolute
+        top: 0
+        right: -15px
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,13 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
+    title: 'Индивидуальный',
     component: Home
   },
   {
     path: '/duble',
     name: 'Duble',
+    title: 'Парный',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
